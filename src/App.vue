f53aa9d9bd0aae6de21bebec8952603b4f06a6f9
MrNobody233 <1356177686@qq.com>
2022-05-07 14:07:32 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="appContainer">
+    <!--1.1 使用A模块的state数据-->
+    <p>{{ $store.state.moduleA.username }}</p>
+
+    <!--1.2 使用A模块的getters数据-->
+    <p>{{ $store.getters.newName }}</p>
+
+    <!--2.1使用B模块的state数据-->
+    <p>{{ $store.state.moduleB.username }}</p>
+
+    <!--2.2 使用B模块的getters数据-->
+    <p>{{ $store.getters['moduleB/newName'] }}</p>
+
+    <!--2.3 使用B模块的mutation数据-->
+    <p>{{ $store.state.moduleB.username }}</p>
+    <button @click="updateName">更改姓名</button>
+    <button @click="asyncUpdateName">更改姓名</button>
+  </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import {useStore} from 'vuex'
 
-nav {
-  padding: 30px;
+export default {
+  name: 'App ',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  setup() {
+    const store = useStore()
 
-    &.router-link-exact-active {
-      color: #42b983;
+    const updateName = () => {
+      // 2.3提交B模块的修改
+      store.commit('moduleB/updateName')
+
     }
+
+    const asyncUpdateName = () => {
+      store.dispatch('moduleB/updateName')
+    }
+
+    return {updateName, asyncUpdateName }
   }
 }
+</script>
+
+<style lang="less">
+
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,76 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+
+// A模块
+const moduleA = {
+    state: {
+        username: 'moduleA'
+    },
+    getters: {
+        newName(state) {
+            return state.username + '!!! '
+        }
+    },
+    mutations: {
+        updateName(state) {
+            state.username = 'ls'
+        }
+    }
+}
+
+// B模块
+const moduleB = {
+    namespaced: true,
+    state: {
+        username: 'moduleB'
+    },
+    getters: {
+        newName(state) {
+            return state.username + '!!! '
+        }
+    },
+    mutations: {
+        updateName(state) {
+            state.username = 'moduleBBBBBBBBBBBBBBBB'
+        }
+    },
+    actions: {
+        updateName(context) {
+            setTimeout(() => {
+                context.commit('updateName')
+            }, 5000)
+        }
+    },
+}
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    modules: {
+        moduleA,
+        moduleB
+    }
 })
+
+/*
+export default createStore({
+    state: {
+        username: 'zs'
+    },
+    getters: {
+        newName(state) {
+            return state.username + '!!!'
+        }
+    },
+    mutations: {
+        updateName(state) {
+            state.username = 'ls'
+        }
+    },
+    actions: {
+        updateName(context) {
+            setTimeout(() => {
+                context.commit('updateName')
+            }, 5000)
+        }
+    },
+    modules: {}
+})
+*/
